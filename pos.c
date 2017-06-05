28dba04b38964e4545f073f325cbf96e66e6fa71
SUBHASANKARI <subhamathi10@gmail.com>
2017-06-05 14:24:35 +0530

    Add files via upload

---
+++ pos.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int num;
+printf("enter the num");
+scanf("%d",&num);
+if(num>=1)
+{printf("the num is pos");
+}
+else if(num<=1)
+{
+printf("the num is neg");}
+else
+{printf("the num is zero");}}
